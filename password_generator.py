8a75ea9a373a18df4efc5c6ee1a9de3615843ea0
ganeshborra <161814525+ganeshborra@users.noreply.github.com>
2024-07-29 21:27:43 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,38 @@
+import random
+import string
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+        
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+    
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+        
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+    return pwd
+            
+user_input = int(input("Enter the desired length of the password:"))
+has_number = input("Do you want to have numbers (y/n)?").lower()=="y"
+has_special = input("Do you want to have special characters(y/n)?").lower()=="y"
+pwd = generate_password(user_input,has_number,has_special)
+print(pwd)
